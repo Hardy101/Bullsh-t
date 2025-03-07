b45706cb695ee49f6bab8284b360b5383d1cc62e
Eghosa Ordia <eghordia130@gmail.com>
2025-03-07 22:19:14 +0100

    refactor: integrate Tailwind CSS and enhance Navbar component styling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,10 +2,23 @@ import { MEDIA } from "../constants/media";
 
 export const NavBar = () => {
   return (
-    <nav>
-      <a href="#">
-        <img src={MEDIA.LOGO} alt="logo" />
-      </a>
+    <nav className="p-4"> 
+      <div className="flex justify-between gap-4 px-6 py-2 text-white font-neucha bg-dark-2 rounded-full">
+        <a href="#">
+          <img src={MEDIA.LOGO} alt="logo" className="w-20" />
+        </a>
+        <ul className="flex gap-4 my-auto text-sm">
+          <li>
+            <a href="#">Forums</a>
+          </li>
+          <li>
+            <a href="#">Create Forum</a>
+          </li>
+          <li>
+            <a href="#">Register</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
