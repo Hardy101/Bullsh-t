9d704682edda5d367c06df08836e57b32e7c238e
Eghosa Ordia <eghordia130@gmail.com>
2025-03-07 22:24:18 +0100

    feat: add input field for forum invite link in Navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,11 +2,20 @@ import { MEDIA } from "../constants/media";
 
 export const NavBar = () => {
   return (
-    <nav className="p-4"> 
+    <nav className="p-4">
       <div className="flex justify-between gap-4 px-6 py-2 text-white font-neucha bg-dark-2 rounded-full">
-        <a href="#">
+        <a href="#" className="my-auto">
           <img src={MEDIA.LOGO} alt="logo" className="w-20" />
         </a>
+        <div className="grow input-control">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Paste forum invite link or type forum name"
+            className="w-full px-3 py-2 rounded-full outline-none border border-dark-1 text-sm text-center"
+          />
+        </div>
         <ul className="flex gap-4 my-auto text-sm">
           <li>
             <a href="#">Forums</a>
