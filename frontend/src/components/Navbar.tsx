3f7015c595485bed37db6499383cc9d8db4827b8
Eghosa Ordia <eghordia130@gmail.com>
2025-03-07 22:07:35 +0100

    add Navbar component with logo integration

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { MEDIA } from "../constants/media";
+
+export const NavBar = () => {
+  return (
+    <nav>
+      <a href="#">
+        <img src={MEDIA.LOGO} alt="logo" />
+      </a>
+    </nav>
+  );
+};
