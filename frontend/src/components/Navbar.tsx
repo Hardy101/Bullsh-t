afc99cabf6134e0af0181c3ef520c73d153f63c8
Eghosa Ordia <eghordia130@gmail.com>
2025-03-08 04:50:52 +0100

    refactor: update Navbar component styling and input field layout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,18 +2,18 @@ import { MEDIA } from "../constants/media";
 
 export const NavBar = () => {
   return (
-    <nav className="p-4">
-      <div className="flex justify-between gap-4 px-6 py-2 text-white font-neucha bg-dark-2 rounded-full">
+    <nav>
+      <div className="flex justify-between gap-4 px-6 py-4 text-white font-neucha  rounded-full">
         <a href="#" className="my-auto">
           <img src={MEDIA.LOGO} alt="logo" className="w-20" />
         </a>
-        <div className="grow input-control">
+        <div className="grow input-control flex justify-center">
           <input
             type="text"
             name=""
             id=""
             placeholder="Paste forum invite link or type forum name"
-            className="w-full px-3 py-2 rounded-full outline-none border border-dark-1 text-sm text-center"
+            className="w-3/5 px-3 py-2 rounded-md outline-none border border-[#B2BEB5] text-xs text-center"
           />
         </div>
         <ul className="flex gap-4 my-auto text-sm">
