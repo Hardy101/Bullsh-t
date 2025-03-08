69161847d42a84ed916c8b00f9cac962e7834a3c
Eghosa Ordia <eghordia130@gmail.com>
2025-03-08 19:03:07 +0100

    feat: remove Niramit font files and update font references in the application

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,30 +3,10 @@ import { MEDIA } from "../constants/media";
 export const NavBar = () => {
   return (
     <nav>
-      <div className="flex justify-between gap-4 px-6 py-4 text-white font-niramit  rounded-full">
+      <div className="flex justify-center">
         <a href="#" className="my-auto">
-          <img src={MEDIA.LOGO} alt="logo" className="w-20" />
+          <img src={MEDIA.BULLSHIT} alt="logo" className="w-20" />
         </a>
-        <div className="grow input-control flex justify-center">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Paste forum invite link or type forum name"
-            className="w-4/5 px-3 py-2 rounded-md outline-none border border-[#B2BEB5] text-xs text-center"
-          />
-        </div>
-        <ul className="flex gap-4 my-auto text-sm">
-          <li>
-            <a href="#">Forums</a>
-          </li>
-          <li>
-            <a href="#">Create Forum</a>
-          </li>
-          <li>
-            <a href="#">Register</a>
-          </li>
-        </ul>
       </div>
     </nav>
   );
--- frontend/src/constants/media.tsx
+++ frontend/src/constants/media.tsx
@@ -1,7 +1,9 @@
 import Logo from "../assets/logo.png";
+import bullshit from "../assets/icon/Bullsh!t.png";
 import Anonym from "../assets/icon/anonymous.png";
 
 export const MEDIA = {
   LOGO: Logo,
+  BULLSHIT: bullshit,
   ANONYM: Anonym,
 };
