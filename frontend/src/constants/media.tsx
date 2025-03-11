798d55d45b18ccc6f07b7bfac69388ec8e0994ae
Eghosa Ordia <eghordia130@gmail.com>
2025-03-11 21:22:06 +0100

    feat: add loading icon to MEDIA constants for enhanced user feedback

--- frontend/src/constants/media.tsx
+++ frontend/src/constants/media.tsx
@@ -2,10 +2,12 @@ import bullshit from "../assets/Bullsh!t.png";
 import sparkling from "../assets/sparkling.png";
 import Anonym from "../assets/icon/anonymous.png";
 import rightArrow from "../assets/icon/right-arrow.png";
+import Loading from "../assets/gifs/icons8-loading.gif";
 
 export const MEDIA = {
   BULLSHIT: bullshit,
   ANONYM: Anonym,
   sparkling: sparkling,
   rightArrow: rightArrow,
+  loadingIcon: Loading,
 };
