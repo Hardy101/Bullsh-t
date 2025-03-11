26cfc703013d93a3e08474e31150a876f5b9ad18
Eghosa Ordia <eghordia130@gmail.com>
2025-03-11 21:06:20 +0100

    feat: add SendMessage page with form handling and ScaleUpDiv integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import GetLink from "./pages/generateLink";
 import MessageBoard from "./pages/msgBoard";
 import EnterForum from "./pages/enterForum";
 import AuthModal from "./components/authModal";
+import SendMessage from "./pages/sendMessage";
 import { NavBar } from "./components/Navbar";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path="getlink" element={<GetLink />} />
           <Route path="messageboard" element={<MessageBoard />} />
           <Route path="forum" element={<EnterForum />} />
+          <Route path="sendMessage" element={<SendMessage />} />
         </Routes>
       </BrowserRouter>
 
--- frontend/src/pages/sendMessage.tsx
+++ frontend/src/pages/sendMessage.tsx
@@ -1,5 +1,56 @@
-import ScaleUpDiv from "../components/scaleupDiv"
+import { useState } from "react";
+import ScaleUpDiv from "../components/scaleupDiv";
+
+interface FormData {
+  message: string;
+  error?: string;
+}
 
 const SendMessage: React.FC = () => {
-    retur
-}
+  const [formData, setFormData] = useState<FormData>({
+    message: "",
+    error: "",
+  });
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <ScaleUpDiv title="Send Message">
+      <form onSubmit={handleSubmit} className="step-1 mt-8">
+        {formData.error && (
+          <div className="form-control font-neucha bg-red-500 py-1 text-center text-sm">
+            <p>{formData.error}</p>
+          </div>
+        )}
+
+        <div className="grid gap-6 mt-8">
+          <div className="flex flex-col gap-2">
+            <textarea
+              name="message"
+              id="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+              placeholder="Enter your message...no one has to know"
+              className="w-full courgette-regular bg-transparent px-3 py-2 text-white placeholder:text-grey border-2 border-grey rounded-lg outline-none"
+            ></textarea>
+            <span className="text-grey text-sm font-neucha">
+              Enter your message.
+            </span>
+          </div>
+          <div>
+            <button type="button" className="font-lobster w-full py-2 bg-purple bs-1 rounded-2xl">Send Message</button>
+          </div>
+        </div>
+      </form>
+    </ScaleUpDiv>
+  );
+};
+
+export default SendMessage;
