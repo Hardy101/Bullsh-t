12dc0ad26f9ec0da7116a5c77033b123bf7af40a
Eghosa Ordia <eghordia130@gmail.com>
2025-03-11 15:30:13 +0100

    feat: add AuthModal component for user authentication interface

---
+++ frontend/src/components/authModal.tsx
@@ -0,0 +1,9 @@
+import { MEDIA } from "../constants/media";
+
+export default function AuthModal() {
+  return <div className="fixed top-0 left-0 w-full h-full bg-black opacity-40">
+    <div className="modal bg-white rounded-2xl p-4">
+        
+    </div>
+  </div>;
+}
