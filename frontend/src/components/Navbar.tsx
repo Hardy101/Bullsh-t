ecd85c492be41154f12e308a7de94977c5a37511
Eghosa Ordia <eghordia130@gmail.com>
2025-03-11 09:04:34 +0100

    feat: add Register/Login button to Navbar for user authentication

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,10 +3,14 @@ import { MEDIA } from "../constants/media";
 export const NavBar = () => {
   return (
     <nav>
-      <div className="fixed w-full top-0 left-0 flex justify-center p-8">
+      <div className="fixed w-full top-0 left-0 flex justify-between p-8">
         <a href="#" className="my-auto">
           <img src={MEDIA.BULLSHIT} alt="logo" className="w-28" />
         </a>
+        <button className="inline-flex items-center py-1 pl-1 pr-2 gap-4 text-black text-sm font-lobster bg-white rounded-full">
+          <img src={MEDIA.ANONYM} alt="anonymous" className="w-8 " />
+          <span>Register/Login</span>
+        </button>
       </div>
     </nav>
   );
